6782cb2388324a6deacd93e21d9f70d141ce2bab
jun-hf <fong.junh@gmail.com>
2024-03-19 16:59:45 +0000

    upgraded to gin server

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"mime"
 	"net/http"
 	"strconv"
 	"time"
 	"os"
+	"github.com/gin-gonic/gin"
 	"github.com/jun-hf/GO_REST_SERVER/internal/db"
 )
 
@@ -20,166 +18,91 @@ func createTodoServer() *ToDoServer {
 	return &ToDoServer{todoDB: tdDB}
 }
 
-func (tdServer *ToDoServer) createTodoHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("processing todo create at %s\n", req.URL.Path)
+func (tdServer *ToDoServer) getAllTodoHandler(c *gin.Context) {
+	allTodos := tdServer.todoDB.GetAllTodos()
+	c.JSON(http.StatusOK, allTodos)
+}
 
+func (tdServer *ToDoServer) deleteAllTodosHandler(c *gin.Context) {
+	tdServer.todoDB.DeleteAllTodos()
+}
+
+func (tdServer *ToDoServer) createTodoHandler(c *gin.Context) {
 	type RequestTodo struct {
-		Description string
-		Tags        []string
-		Due         time.Time
+		Description string `json:"description"`
+		Tags []string `json:"tags"`
+		Due time.Time `json:"due"`
 	}
 
-	type ResponseId struct {
-		Id int
-	}
-
-	// ensure is application/json
-	contentType := req.Header.Get("Content-Type")
-	mediaType, _, err := mime.ParseMediaType(contentType)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	if mediaType != "application/json" {
-		http.Error(w, "expecting application/json", http.StatusUnsupportedMediaType)
-		return
-	}
-
-	decode := json.NewDecoder(req.Body)
-	decode.DisallowUnknownFields()
 	var reqTodo RequestTodo
-	if err := decode.Decode(&reqTodo); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	if err := c.ShouldBindJSON(&reqTodo); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
 		return
 	}
 
 	id := tdServer.todoDB.CreateTodo(reqTodo.Description, reqTodo.Tags, reqTodo.Due)
-	
-	js, err := json.Marshal(ResponseId{Id: id})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	c.JSON(http.StatusOK, gin.H{"id": id})
 }
 
-func (tdServer *ToDoServer) getAllTodosHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("processing getAllTodosHandler at %s\n", req.URL.Path)
-
-	todosList := tdServer.todoDB.GetAllTodos()
-	js, err := json.Marshal(todosList)
+func (tdServer *ToDoServer) getTodoHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Params.ByName("id"))
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
-}
-
-func (tdServer *ToDoServer) robotHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("processing robotHandler at %s\n", req.URL.Path)
-	w.Write([]byte("Heelo"))
-}
-
-func (tdServer *ToDoServer) getTodoHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("handling getTodoHandler at %s\n", req.URL.Path)
-
-	id, err := strconv.Atoi(req.PathValue("id"))
-	if err != nil {
-		http.Error(w, "invalid id", http.StatusBadRequest)
+		c.String(http.StatusBadRequest, err.Error())
 		return
 	}
 
 	todo, err := tdServer.todoDB.GetTodo(id)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
+		c.String(http.StatusNotFound, err.Error())
 		return
 	}
 
-	js, err := json.Marshal(todo)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	c.JSON(http.StatusOK, todo)
 }
 
-func (tdServer *ToDoServer) deleteTodoHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("handling deleteTodoHandler at %s\n", req.URL.Path)
-
-	id, err := strconv.Atoi(req.PathValue("id"))
+func (tdServer *ToDoServer) deleteTodoHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Params.ByName("id"))
 	if err != nil {
-		http.Error(w, "invalid id please provide a valid integer", http.StatusBadRequest)
+		c.String(http.StatusBadRequest, err.Error())
 		return
 	}
 
-	err = tdServer.todoDB.DeleteTodo(id)
-	if err != nil {
-		http.Error(w, "id does not exist in db", http.StatusBadRequest)
+	if err = tdServer.todoDB.DeleteTodo(id); err != nil {
+		c.String(http.StatusNotFound, err.Error())
+		return
 	}
 }
 
-func (tdServer *ToDoServer) deleteAllTodosHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("Deleting all todos at %s\n", req.URL.Path)
-	tdServer.todoDB.DeleteAllTodos()
-}
-
-func (tdServer *ToDoServer) getByTagHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("Getting by tags at %s\n", req.URL.Path)
-	tag := req.PathValue("tag")
-
+func (tdServer *ToDoServer) tagHandler(c *gin.Context) {
+	tag := c.Params.ByName("tag")
 	todos := tdServer.todoDB.GetTodoByTag(tag)
-	
-	js, err := json.Marshal(todos)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	c.JSON(http.StatusOK, todos)
 }
 
-func (tdServer *ToDoServer) getByDueDateHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("Getting By Due date %s\n", req.URL.Path)
-
-	writeJsonResponse := func(w http.ResponseWriter, response []byte) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(response)
-	}
-
-	year, errYear := strconv.Atoi(req.PathValue("year"))
-	month, errMonth := strconv.Atoi(req.PathValue("month"))
-	day, errDay := strconv.Atoi(req.PathValue("day"))
+func (tdServer *ToDoServer) dueHandler(c *gin.Context) {
+	year, errYear := strconv.Atoi(c.Params.ByName("year"))
+	month, errMonth := strconv.Atoi(c.Params.ByName("month"))
+	day, errDay := strconv.Atoi(c.Params.ByName("day"))
 	if errYear != nil || errMonth !=nil || errDay !=nil || month < int(time.January) || month > int(time.December) {
-		http.Error(w, "Invalid request format expect: /year/month/day in int", http.StatusBadRequest)
+		c.String(http.StatusBadRequest, "expect /due/<year>/<month>/<day>, got %v", c.FullPath())
 		return
 	}
 
 	todos := tdServer.todoDB.GetTodosByDueDate(year, time.Month(month), day)
-	js, err := json.Marshal(todos)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	writeJsonResponse(w, js)
+	c.JSON(http.StatusOK, todos)
 }
 
 func main() {
-	mux := http.NewServeMux()
-	todoServer := createTodoServer()
+	router := gin.Default()
+	server := createTodoServer()
 
-	mux.HandleFunc("POST /todo/", todoServer.createTodoHandler)
-	mux.HandleFunc("GET /todos/", todoServer.getAllTodosHandler)
-	mux.HandleFunc("GET /robot.txt", todoServer.robotHandler)
-	mux.HandleFunc("GET /todo/{id}", todoServer.getTodoHandler)
-	mux.HandleFunc("DELETE /todo/{id}", todoServer.deleteTodoHandler)
-	mux.HandleFunc("DELETE /deleteAllTodos", todoServer.deleteAllTodosHandler)
-	mux.HandleFunc("GET /tag/{tag}", todoServer.getByTagHandler)
-	mux.HandleFunc("GET /due/{year}/{month}/{day}", todoServer.getByDueDateHandler)
+	router.POST("/todos", server.createTodoHandler)
+	router.GET("/todos", server.getAllTodoHandler)
+	router.DELETE("/todos/deleteAll", server.deleteAllTodosHandler)
+	router.GET("/todos/{id}", server.getTodoHandler)
+	router.DELETE("/todos/{id}", server.deleteTodoHandler)
+	router.GET("/tag", server.tagHandler)
+	router.GET("/due/{year}/{month}/{day}", server.dueHandler)
 
-	log.Fatal(http.ListenAndServe("localhost:"+os.Getenv("PORT"), mux))
+	router.Run("localhost:" + os.Getenv("PORT"))
 }
--- manual_testing.sh
+++ manual_testing.sh
@@ -4,11 +4,10 @@ PORT=3000
 ADDRESS=localhost:${PORT}
 
 # Start by deleting all existing todos on the server
-curl -iL -w "\n" -X DELETE ${ADDRESS}/deleteAllTodos
+curl -iL -w "\n" -X DELETE ${ADDRESS}/todos/deleteAll
 
 # Add some todos
-curl -iL -w "\n" -X POST -H "Content-Type: application/json" --data '{"Description":"Write Code","tags":["coding"], "due":"2023-01-02T15:04:05+00:00"}' ${SERVERADDR}/todo/
-curl -iL -w "\n" -X POST -H "Content-Type: application/json" --data '{"Description":"Learn Go","tags":["career"], "due":"2024-01-03T15:04:05+00:00"}' ${SERVERADDR}/todo/
+curl -iL -X POST -H "Content-Type: application/json" -d '{"description":"Write Code","tags":["coding"],"due":"2023-01-03T15:04:05+00:00"}' ${SERVERADDR}/todo/
 
 # Get todos by tag
 curl -iL -w "\n" ${ADDRESS}/tag/coding/
